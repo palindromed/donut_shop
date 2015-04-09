bba7273ae9e44c3db78a45eaa2dbf00d0e2e4466
Hannah Krager <hannahkrager@gmail.com>
2015-04-08 20:13:15 -0700

    Basic CSS

--- donuts.js
+++ donuts.js
@@ -34,11 +34,11 @@
 
   var existingShops = function() {
     if (array.length === 0) {
-      array[0] = new DonutShop({shop: 'Downtown', min: 8, max: 43, avg: 4.5});
-      array[1] = new DonutShop({shop :'Capitol Hill', min: 4, max: 37, avg: 2.0});
-      array[2] = new DonutShop({shop :'South Lake Union', min: 9, max: 23, avg: 6.33});
-      array[3] = new DonutShop({shop:'Wedgewood', min: 2, max: 28, avg: 1.25});
-      array[4] = new DonutShop({shop:'Ballard', min: 8, max: 58, avg: 3.75});
+      array.push(new DonutShop({shop: 'Downtown', min: 8, max: 43, avg: 4.5}));
+      array.push(new DonutShop({shop :'Capitol Hill', min: 4, max: 37, avg: 2.0}));
+      array.push(new DonutShop({shop :'South Lake Union', min: 9, max: 23, avg: 6.33}));
+      array.push(new DonutShop({shop:'Wedgewood', min: 2, max: 28, avg: 1.25}));
+      array.push(new DonutShop({shop:'Ballard', min: 8, max: 58, avg: 3.75}));
     }
     else {
       return;
