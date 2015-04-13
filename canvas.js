3e1862031d5d42b628086dcb1f2e16d2a7599495
Hannah Krager <hannahkrager@gmail.com>
2015-04-13 10:19:37 -0700

    canvas js code

---
+++ canvas.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function(){
+  var canvas1 = document.getElementById('canvas1');
+  var context= canvas1.getContext('2d');
+
+  context.setStrokeColor(0, 0, 255, 255);
+  context.fillRect(10, 10, 20, 20);
+
+
+})();
